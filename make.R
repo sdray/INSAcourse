0e443ebd312b0bc9fce119b03ef645c631da0cad
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2022-01-07 13:42:45 +0100

    Update session8 in session7 for compilation

--- make.R
+++ make.R
@@ -22,9 +22,10 @@ rmarkdown::render(here::here("practical/session4","session4.Rmd"),
                   params = list(correction = TRUE), output_file = "session4-corrected")
 
 rmarkdown::render(here::here("practical/session6","session6.Rmd"))
-rmarkdown::render(here::here("practical/session8","session8.Rmd"))
-rmarkdown::render(here::here("practical/session8","session8.Rmd"),
-                  params = list(correction = TRUE), output_file = "session8-corrected")
+
+rmarkdown::render(here::here("practical/session7","session7.Rmd"))
+rmarkdown::render(here::here("practical/session7","session7.Rmd"),
+                  params = list(correction = TRUE), output_file = "session7-corrected")
 
 ## build pdf
 pagedown::chrome_print(here::here("course/session1","session1.html"))
@@ -35,5 +36,5 @@ pagedown::chrome_print(here::here("practical/session3","session3-corrected.html"
 pagedown::chrome_print(here::here("practical/session4","session4.html"))
 pagedown::chrome_print(here::here("practical/session4","session4-corrected.html"))
 pagedown::chrome_print(here::here("practical/session6","session6.html"))
-pagedown::chrome_print(here::here("practical/session8","session8.html"))
-pagedown::chrome_print(here::here("practical/session8","session8-corrected.html"))
+pagedown::chrome_print(here::here("practical/session7","session7.html"))
+pagedown::chrome_print(here::here("practical/session7","session7-corrected.html"))
